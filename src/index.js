e05b737d2aec6c8195829506688311032f656e6e
cwic0864 <cwic0864@sysco.com>
2019-10-05 09:40:15 +0530

    Add answers up to 3rd task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,73 @@
-const { getRandomWordSync, getRandomWord } = require('word-maker');
+const {getRandomWordSync, getRandomWord} = require('word-maker');
 
-console.log('It works!');
+const taskOne = () => {
+    for (let i = 1; i < 101; i++) {
+        console.log(`${i}: ${getRandomWordSync()}`)
+    }
+};
 
-// YOUR CODE HERE
+const taskTwo = () => {
+    for (let i = 1; i < 101; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log(`${i}: FizzBuzz`)
+        } else if (i % 3 === 0) {
+            console.log(`${i}: Fizz`)
+        } else if (i % 5 === 0) {
+            console.log(`${i}: Buzz`)
+        } else {
+            console.log(`${i}: ${getRandomWordSync()}`)
+        }
+    }
+};
+
+const taskThreeV1 = () => {
+    const response = [];
+
+    for (let i = 1; i < 101; i++) {
+        response.push(getRandomWord());
+    }
+
+    Promise.all(response).then(words => {
+        words.forEach((word, index) => {
+                console.log(`${index + 1}: ${word}`);
+            }
+        )
+    })
+};
+
+const taskThreeV2 = () => {
+    const response = [];
+
+    for (let i = 1; i < 101; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            response.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            response.push("Fizz");
+        } else if (i % 5 === 0) {
+            response.push("Buzz");
+        } else {
+            response.push(getRandomWord());
+        }
+    }
+
+    Promise.all(response).then(words => {
+        words.forEach((word, index) => {
+                console.log(`${index + 1}: ${word}`);
+            }
+        )
+    })
+};
+
+
+// Execution of task one
+// taskOne();
+
+// Execution of task two
+// taskTwo();
+
+// Execution of task three version 1
+// taskThreeV1();
+
+// Execution of task three version 2
+taskThreeV2();
+
